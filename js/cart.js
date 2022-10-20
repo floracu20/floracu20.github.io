77a1edd46a85fd9b9460390719d82167538db675
floracu <floracuna20@gmail.com>
2022-10-20 20:02:51 -0300

    Entrega 5 - Carrito de compras
    Solo para usuario "25801"

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,82 @@
+const URL = CART_INFO_URL + "25801" + EXT_TYPE;
+let productosAComprar = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+    getJSONData(URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            console.log(resultObj);
+            productosAComprar = resultObj.data.articles; /*Asigno los productos en el carrito del .json, al array*/
+            showCart();
+            }       
+    });
+});
+
+/*Función que muestra el contenido del carrito*/
+function showCart() {
+    let htmlContenidoCarrito = "";
+    let htmlContenidoProd = "";
+
+    for (let i = 0; i < productosAComprar.length; i++) {  /*En esta entrega va a mostrar un solo artículo*/
+        let datos = productosAComprar[i];
+        htmlContenidoProd += `
+        <div class="list-group-item list-group-item-action" style="text-align: center">
+            <div class="row">
+                <div class="col-md-2">  
+                    <img src="${datos.image}" class="img-responsive" width="50%" height="80%">  
+                </div>  
+                <div class="col-md-2">    
+                    <p class="mb-1">${datos.name}</p>
+                </div>  
+                <div class="col-md-2"> 
+                    <p class="mb-1">${datos.currency} ${datos.unitCost}</p>
+                </div>  
+                <div class="col-md-2"> 
+                    <input type="number" min="0" id="cantidadArticulos" name="cantidadArticulos" style="width:50px" onchange="multiplicar(this.value, ${datos.unitCost}, '${datos.currency}')" value="${datos.count}">
+                </div>  
+                <div class="col-md-2"> 
+                    <p class="mb-1" id="subtotal">${datos.currency} ${datos.unitCost}</p>
+                </div>
+            </div>
+        </div>
+        `
+    };
+
+    htmlContenidoCarrito += `
+    <hr/>
+    <div class="list-group-item" style="text-align: center; background-color: #a7e7ec" >
+        <div class="row">
+            <div class="col-md-2">
+                <h5 class="col-3 bg-light"></h5>
+            </div>
+            <div class="col-md-2">
+                <h5>Nombre</h5>
+            </div>
+            <div class="col-md-2">
+                <h5>Costo</h5>
+            </div>
+            <div class="col-md-2">           
+                <h5>Cantidad</h5>
+            </div>
+            <div class="col-md-2">
+                <h5>Subtotal</h5>
+            </div>
+            <hr size="5" style="background-color: #005706; height: 2px">
+                ${htmlContenidoProd}
+        </div>
+    </div> 
+    `
+        document.getElementById("comp-list-container").innerHTML = htmlContenidoCarrito;
+   
+}
+
+/* Función que multiplica el precio unitario del artículo, por el valor del input "cantidadArticulos" */
+
+function multiplicar(cantidad, precioUnitario, currency) {
+    var subtotal = cantidad * precioUnitario;
+    document.getElementById("subtotal").innerHTML = currency + " " + subtotal;
+}
+
+
 //Muestra el nombre de usuario con menú desplegable en la barra superior, pero en caso de no tener usuario muestra Login
 const menu = document.getElementById("desplegable_login");
 if (localStorage.getItem('user') == undefined) {
