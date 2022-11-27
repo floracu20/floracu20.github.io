2bcdb1bb57cdd19ff4b2f2fb96b1fee0f53f2428
floracu <floracuna20@gmail.com>
2022-11-27 16:27:07 -0300

    entrega 7 + desafiate
    Sección Mi perfil + carga de foto de perfil.

--- js/login.js
+++ js/login.js
@@ -1,10 +1,10 @@
 const button = document.getElementById("ingBtn");
 
 function inicio(){
-    const email = document.getElementById("email").value;
-    const pass = document.getElementById("pass").value;
+    var email = document.getElementById("email").value;
+    var pass = document.getElementById("pass").value;
 
-    if ((email.length) && (pass.length)){
+    if ((email.length) && (pass.length) && (email.includes('@'))){
         window.location = "index.html";
     }
 }
@@ -13,6 +13,6 @@ button.onclick = inicio;
 inicio();
 
 document.getElementById("ingBtn").addEventListener("click", ()=>{
-    localStorage.setItem("user", document.getElementById("email").value)
+    localStorage.setItem("user", document.getElementById("email").value);
     document.getElementById("mostrarUsuario").innerHTML = localStorage.getItem("user");
 })
--- js/my-profile.js
+++ js/my-profile.js
@@ -1,3 +1,92 @@
+/* Función que valida que ningún campo quede vacío y guarda los datos */
+function validarDatos() {
+    let nombre = document.getElementById("nombre1");
+    let apellido = document.getElementById("apellido1");
+    let nroTeléfono = document.getElementById("teléfono");
+    let campoVacio = false;
+
+    /* Valido campos vacíos */
+    if (nombre.value === '') {
+        nombre.classList.add('is-invalid');
+        campoVacio = true;
+    }
+
+    if (apellido.value === '') {
+        apellido.classList.add('is-invalid');
+        campoVacio = true;
+    }
+
+    if (nroTeléfono.value === '') {
+        nroTeléfono.classList.add('is-invalid');
+        campoVacio = true;
+    }
+
+    if (!campoVacio) {
+        GuardarDatosUser();
+    }
+
+}
+
+/* Función que quita el mensaje de error cuando se completa el campo */
+function removerInvalido(id) {
+    let inputError = document.getElementById(id);
+    inputError.classList.remove('is-invalid');
+}
+ 
+/* FIN VALIDACIÓN */
+
+
+function GuardarDatosUser() {
+    let nombre = document.getElementById("nombre1").value;
+    let apellido = document.getElementById("apellido1").value;
+    let nroTel = document.getElementById("teléfono").value;
+    let segundoNombre = document.getElementById("nombre2").value;
+    let segundoApellido = document.getElementById("apellido2").value;
+
+    /* Acá guardo los datos en localStorage */
+    localStorage.setItem("inputNombre1", nombre);
+    localStorage.setItem("inputApellido1", apellido);
+    localStorage.setItem("inputNroTeléfono", nroTel);
+    localStorage.setItem("inputNombre2", segundoNombre);
+    localStorage.setItem("inputApellido2", segundoApellido);
+
+}
+
+
+/* Función que muestra los datos del usuario en los inputs */
+function showDatos() {
+    /* Muestro los datos ingresados y guardados en LocalStorage */
+    document.getElementById('nombre1').value = localStorage.getItem("inputNombre1");
+    document.getElementById('apellido1').value = localStorage.getItem("inputApellido1");
+    document.getElementById('teléfono').value = localStorage.getItem("inputNroTeléfono");
+    document.getElementById('nombre2').value = localStorage.getItem("inputNombre2");
+    document.getElementById('apellido2').value = localStorage.getItem("inputApellido2");
+    document.getElementById('correo').value = localStorage.getItem('user');
+    document.getElementById("display_image").src = localStorage.getItem("foto");
+    /* Muestro foto de perfil predeterminada si el usuario no tiene una imagen cargada (desafiate): */
+    if (localStorage.getItem("foto") == null) {
+        document.getElementById("display_image").src = fotoPerfil
+    }
+}
+
+
+/* Para la foto de perfil (desafiate): */
+const image_input = document.querySelector("#image_input");
+var uploaded_image = "";
+let fotoPerfil = "img/img_perfil.png";
+
+/* Actualiza la foto de perfil */
+image_input.addEventListener("change", function () {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+        uploaded_image = reader.result;
+        localStorage.setItem("foto", uploaded_image);
+        document.getElementById("display_image").src = localStorage.getItem("foto")
+    });
+    reader.readAsDataURL(this.files[0]);
+})
+
+
 //Muestra el nombre de usuario con menú desplegable en la barra superior, pero en caso de no tener usuario muestra Login
 const menu = document.getElementById("desplegable_login");
 if (localStorage.getItem('user') == undefined) {
@@ -17,9 +106,16 @@ if (localStorage.getItem('user') == undefined) {
         </li>
         `
     menu.innerHTML = desplegable;
+    showDatos();
 }
 
 
-function cierraSesion(){
+function cierraSesion() {
     localStorage.removeItem("user");
+    localStorage.removeItem("inputNombre1");
+    localStorage.removeItem("inputApellido1");
+    localStorage.removeItem("inputNroTeléfono");
+    localStorage.removeItem("inputNombre2");
+    localStorage.removeItem("inputApellido2");
+    localStorage.removeItem("foto");
 };
